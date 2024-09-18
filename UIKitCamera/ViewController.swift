60fdfd8ff3dc3d412f262c88a02fd11d7e4f7e56
Tim Mitra <tim@it-guy.com>
2024-09-18 00:07:35 -0400

    Initial Commit

---
+++ UIKitCamera/ViewController.swift
@@ -0,0 +1,23 @@
+//
+// ---------------------------- //
+// Original Project: UIKitCamera
+// Created on 2024-09-18 by Tim Mitra
+// Mastodon: @timmitra@mastodon.social
+// Twitter/X: timmitra@twitter.com
+// Web site: https://www.it-guy.com
+// ---------------------------- //
+// Copyright © 2024 iT Guy Technologies. All rights reserved.
+
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
